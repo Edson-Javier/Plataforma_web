7eead150b2cdc89292fa6badf563ab0446c9c8c8
Edson-Javier <edson.mendiola4983@alumnos.udg.mx>
2025-11-19 05:53:28 -0600

    Update empleados.sql

--- empleados.sql
+++ empleados.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 23-10-2025 a las 01:27:03
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 19-11-2025 a las 12:51:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,7 +32,7 @@ CREATE TABLE `lista` (
   `nombre` varchar(100) NOT NULL,
   `apellido` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
-  `pass` varchar(32) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `rol` int(11) NOT NULL,
   `imagen` varchar(255) DEFAULT NULL,
   `eliminar` int(11) NOT NULL DEFAULT 0
@@ -43,8 +43,11 @@ CREATE TABLE `lista` (
 --
 
 INSERT INTO `lista` (`id`, `nombre`, `apellido`, `correo`, `pass`, `rol`, `imagen`, `eliminar`) VALUES
-(1, 'Edson Javeir', 'Mendiola Magana', 'edson@hotmil.com', 'edson1234', 1, NULL, 0),
-(2, 'Angel Omar', 'Mendiola Magana', 'omar@hotmail.com', 'omar1234', 2, NULL, 0);
+(1, 'Edson Javier', 'Mendiola MagaÃ±a', 'edson@hotmail.com', '$2y$10$ulKe9iSDb9Zz7sDAljlgzuoSG/RJIu3rKZFSyKIgShgJL5/ubtrie', 1, '8d5a5c0ca8019d53f67d17990b38d6d4.png', 1),
+(2, 'Angel Omar', 'Mendiola Magana', 'omar@hotmail.com', 'omar1234', 2, '04f23d26197195eca186d887debadb35.png', 0),
+(3, 'Sofia Luna', 'Lopez', 'sofia@hotmail.com', '$2y$10$s1o1xNiE96Ce4A6KSM/SaOTGL38bp9gw4D8DJR5sRtePMvgahyb2y', 2, '5a699eed44c51f4e2a1e8ab5ef023c24.webp', 0),
+(4, 'Karla Lucia', 'Rodriguez Macias', 'karla@hotmail.com', '$2y$10$cZFLGN/4E6OFwzWHOtuLFufK.dSk2.ERHMLb42pWjWk3NGW6bj6H2', 1, '2f745c8e223bb24f20a691d5770f75c5.avif', 0),
+(5, 'Joel Luis', 'Zapata', 'joel@gmail.com', '$2y$10$eU4xDE4mxc72xbXIRHjxluFY2gTrc3cnUKnxC264LZ4mjxjnXH9Hq', 1, '4bf0d8a8d1a1f46286ad123ca969de73.jpg', 0);
 
 --
 -- Índices para tablas volcadas
@@ -64,7 +67,7 @@ ALTER TABLE `lista`
 -- AUTO_INCREMENT de la tabla `lista`
 --
 ALTER TABLE `lista`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
